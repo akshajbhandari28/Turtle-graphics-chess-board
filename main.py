b0d190c030562ce6c9d9d1e2c0a27529c61ff10a
Akshaj Bhandari <84961610+akshajbhandari28@users.noreply.github.com>
2022-06-09 11:47:29 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,526 @@
+import turtle
+
+turtle.turtles()
+turtle.speed(100)
+
+
+#    board
+
+turtle.forward(160)
+turtle.left(180)
+turtle.forward(320)
+turtle.right(90)
+turtle.forward(320)
+turtle.right(90)
+turtle.forward(320)
+turtle.right(90)
+turtle.forward(320)
+turtle.left(180)
+
+# ranks
+
+turtle.forward(40)
+turtle.left(90)
+turtle.forward(320)
+turtle.right(90)
+turtle.forward(40)
+turtle.right(90)
+turtle.forward(320)
+
+turtle.left(90)
+turtle.forward(40)
+turtle.left(90)
+turtle.forward(320)
+turtle.right(90)
+turtle.forward(40)
+turtle.right(90)
+turtle.forward(320)
+
+turtle.left(90)
+turtle.forward(40)
+turtle.left(90)
+turtle.forward(320)
+turtle.right(90)
+turtle.forward(40)
+turtle.right(90)
+turtle.forward(320)
+
+turtle.left(90)
+turtle.forward(40)
+turtle.left(90)
+turtle.forward(320)
+turtle.right(90)
+turtle.forward(40)
+turtle.right(90)
+turtle.forward(320)
+
+#   files
+
+turtle.left(180)
+turtle.forward(40)
+
+turtle.left(90)
+turtle.forward(320)
+turtle.right(90)
+turtle.forward(40)
+turtle.right(90)
+turtle.forward(320)
+
+turtle.left(90)
+turtle.forward(40)
+turtle.left(90)
+turtle.forward(320)
+turtle.right(90)
+turtle.forward(40)
+turtle.right(90)
+turtle.forward(320)
+
+turtle.left(90)
+turtle.forward(40)
+turtle.left(90)
+turtle.forward(320)
+turtle.right(90)
+turtle.forward(40)
+turtle.right(90)
+turtle.forward(320)
+
+turtle.left(90)
+turtle.forward(40)
+turtle.left(90)
+turtle.forward(320)
+turtle.right(90)
+turtle.forward(40)
+turtle.right(90)
+turtle.forward(320)
+
+turtle.right(90)
+turtle.forward(40)
+turtle.left(90)
+
+
+## black squares
+## rank 8
+turtle.fillcolor("black")
+turtle.begin_fill()
+for i in range (4):
+    turtle.right(90)
+    turtle.forward(40)
+turtle.end_fill()
+
+
+turtle.right(90)
+turtle.forward(80)
+turtle.left(90)
+
+turtle.begin_fill()
+for i in range (4):
+    turtle.right(90)
+    turtle.forward(40)
+turtle.end_fill()
+
+
+turtle.right(90)
+turtle.forward(80)
+turtle.left(90)
+
+turtle.begin_fill()
+for i in range (4):
+    turtle.right(90)
+    turtle.forward(40)
+turtle.end_fill()
+
+turtle.right(90)
+turtle.forward(80)
+turtle.left(90)
+
+turtle.begin_fill()
+for i in range (4):
+    turtle.right(90)
+    turtle.forward(40)
+turtle.end_fill()
+
+turtle.right(180)
+turtle.forward(80)
+turtle.right(90)
+turtle.forward(40)
+turtle.right(90)
+turtle.forward(40)
+
+## rank 7
+
+turtle.begin_fill()
+for i in range (4):
+    turtle.right(90)
+    turtle.forward(40)
+turtle.end_fill()
+
+turtle.left(90)
+turtle.forward(80)
+turtle.right(90)
+
+turtle.begin_fill()
+for i in range (4):
+    turtle.right(90)
+    turtle.forward(40)
+turtle.end_fill()
+
+turtle.left(90)
+turtle.forward(80)
+turtle.right(90)
+
+turtle.begin_fill()
+for i in range (4):
+    turtle.right(90)
+    turtle.forward(40)
+turtle.end_fill()
+
+turtle.left(90)
+turtle.forward(80)
+turtle.right(90)
+
+
+turtle.begin_fill()
+for i in range (4):
+    turtle.right(90)
+    turtle.forward(40)
+turtle.end_fill()
+
+turtle.left(180)
+turtle.forward(40)
+turtle.right(180)
+turtle.right(90)
+turtle.forward(40)
+turtle.left(90)
+
+# rank 6
+
+turtle.begin_fill()
+for i in range (4):
+    turtle.right(90)
+    turtle.forward(40)
+turtle.end_fill()
+
+turtle.right(90)
+turtle.forward(80)
+turtle.left(90)
+
+turtle.begin_fill()
+for i in range (4):
+    turtle.right(90)
+    turtle.forward(40)
+turtle.end_fill()
+
+turtle.right(90)
+turtle.forward(80)
+turtle.left(90)
+
+turtle.begin_fill()
+for i in range (4):
+    turtle.right(90)
+    turtle.forward(40)
+turtle.end_fill()
+
+turtle.right(90)
+turtle.forward(80)
+turtle.left(90)
+
+turtle.begin_fill()
+for i in range (4):
+    turtle.right(90)
+    turtle.forward(40)
+turtle.end_fill()
+
+turtle.right(180)
+turtle.forward(80)
+turtle.right(90)
+turtle.forward(40)
+turtle.right(90)
+turtle.forward(40)
+
+## rank 5
+
+turtle.begin_fill()
+for i in range (4):
+    turtle.right(90)
+    turtle.forward(40)
+turtle.end_fill()
+
+turtle.left(90)
+turtle.forward(80)
+turtle.right(90)
+
+turtle.begin_fill()
+for i in range (4):
+    turtle.right(90)
+    turtle.forward(40)
+turtle.end_fill()
+
+turtle.left(90)
+turtle.forward(80)
+turtle.right(90)
+
+turtle.begin_fill()
+for i in range (4):
+    turtle.right(90)
+    turtle.forward(40)
+turtle.end_fill()
+
+turtle.left(90)
+turtle.forward(80)
+turtle.right(90)
+
+turtle.begin_fill()
+for i in range (4):
+    turtle.right(90)
+    turtle.forward(40)
+turtle.end_fill()
+
+turtle.left(180)
+turtle.forward(40)
+turtle.right(180)
+turtle.right(90)
+turtle.forward(40)
+turtle.left(90)
+
+
+## rank 4
+
+turtle.begin_fill()
+for i in range (4):
+    turtle.right(90)
+    turtle.forward(40)
+turtle.end_fill()
+
+turtle.right(90)
+turtle.forward(80)
+turtle.left(90)
+
+turtle.begin_fill()
+for i in range (4):
+    turtle.right(90)
+    turtle.forward(40)
+turtle.end_fill()
+
+turtle.right(90)
+turtle.forward(80)
+turtle.left(90)
+
+turtle.begin_fill()
+for i in range (4):
+    turtle.right(90)
+    turtle.forward(40)
+turtle.end_fill()
+
+turtle.right(90)
+turtle.forward(80)
+turtle.left(90)
+
+turtle.begin_fill()
+for i in range (4):
+    turtle.right(90)
+    turtle.forward(40)
+
+turtle.end_fill()
+turtle.right(180)
+turtle.forward(40)
+turtle.right(90)
+turtle.forward(40)
+turtle.right(90)
+
+## rank  3
+
+turtle.fillcolor("black")
+turtle.begin_fill()
+for i in range (4):
+    turtle.right(90)
+    turtle.forward(40)
+turtle.end_fill()
+
+turtle.left(90)
+turtle.forward(80)
+turtle.right(90)
+
+turtle.begin_fill()
+for i in range (4):
+    turtle.right(90)
+    turtle.forward(40)
+turtle.end_fill()
+
+turtle.left(90)
+turtle.forward(80)
+turtle.right(90)
+
+turtle.begin_fill()
+for i in range (4):
+    turtle.right(90)
+    turtle.forward(40)
+turtle.end_fill()
+
+turtle.left(90)
+turtle.forward(80)
+turtle.right(90)
+
+turtle.begin_fill()
+for i in range (4):
+    turtle.right(90)
+    turtle.forward(40)
+turtle.end_fill()
+
+turtle.right(180)
+turtle.forward(40)
+turtle.left(90)
+turtle.forward(40)
+turtle.left(90)
+
+# rank 2
+
+turtle.begin_fill()
+for i in range (4):
+    turtle.right(90)
+    turtle.forward(40)
+turtle.end_fill()
+
+turtle.right(90)
+turtle.forward(80)
+turtle.left(90)
+
+turtle.begin_fill()
+for i in range (4):
+    turtle.right(90)
+    turtle.forward(40)
+turtle.end_fill()
+
+turtle.right(90)
+turtle.forward(80)
+turtle.left(90)
+
+turtle.begin_fill()
+for i in range (4):
+    turtle.right(90)
+    turtle.forward(40)
+turtle.end_fill()
+
+turtle.right(90)
+turtle.forward(80)
+turtle.left(90)
+
+turtle.begin_fill()
+for i in range (4):
+    turtle.right(90)
+    turtle.forward(40)
+turtle.end_fill()
+
+turtle.right(180)
+turtle.forward(40)
+turtle.right(90)
+turtle.forward(40)
+turtle.right(90)
+
+## rank 1
+
+turtle.begin_fill()
+for i in range (4):
+    turtle.right(90)
+    turtle.forward(40)
+turtle.end_fill()
+
+turtle.left(90)
+turtle.forward(80)
+turtle.right(90)
+
+turtle.begin_fill()
+for i in range (4):
+    turtle.right(90)
+    turtle.forward(40)
+turtle.end_fill()
+
+turtle.left(90)
+turtle.forward(80)
+turtle.right(90)
+
+turtle.begin_fill()
+for i in range (4):
+    turtle.right(90)
+    turtle.forward(40)
+turtle.end_fill()
+
+turtle.left(90)
+turtle.forward(80)
+turtle.right(90)
+
+turtle.begin_fill()
+for i in range (4):
+    turtle.right(90)
+    turtle.forward(40)
+turtle.end_fill()
+
+# nubering ranks and files
+
+turtle.penup()
+turtle.left(90)
+turtle.forward(20)
+turtle.left(90)
+turtle.forward(35)
+turtle.write("1", font= 25)
+
+turtle.right(180)
+turtle.forward(40)
+turtle.write("2", font= 25)
+
+turtle.forward(40)
+turtle.write("3", font= 25)
+
+turtle.forward(40)
+turtle.write("4", font= 25)
+
+turtle.forward(40)
+turtle.write("5", font= 25)
+
+turtle.forward(40)
+turtle.write("6", font= 25)
+
+turtle.forward(40)
+turtle.write("7", font= 25)
+
+turtle.forward(40)
+turtle.write("8", font= 25)
+
+turtle.right(180)
+turtle.forward(315)
+turtle.left(90)
+turtle.forward(35)
+
+turtle.write("A", font= 25)
+turtle.forward(40)
+
+turtle.write("B", font= 25)
+turtle.forward(40)
+
+turtle.write("C", font= 25)
+turtle.forward(40)
+
+turtle.write("D", font= 25)
+turtle.forward(40)
+
+turtle.write("E", font= 25)
+turtle.forward(40)
+
+turtle.write("F", font= 25)
+turtle.forward(40)
+
+turtle.write("G", font= 25)
+turtle.forward(40)
+
+turtle.write("H", font= 25)
+turtle.forward(40)
+
+turtle.left(180)
+turtle.forward(250)
+turtle.left(90)
+turtle.forward(60)
+turtle.right(180)
+turtle.forward(20)
+turtle.write("CHESS BOARD", font=100)
+turtle.hideturtle()
+
+turtle.done()
